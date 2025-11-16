2cb848e9335e54dc4ba48d72a1245baced3e1ee4
Gabi2175 <111075042+Gabi2175@users.noreply.github.com>
2025-11-16 23:38:45 +0000

    Implementa microsserviço B: currency-history

---
+++ currency-history/src/main/java/com/example/currencyhistory/CurrencyHistoryApplication.java
@@ -0,0 +1,11 @@
+package com.example.currencyhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyHistoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyHistoryApplication.class, args);
+    }
+}
