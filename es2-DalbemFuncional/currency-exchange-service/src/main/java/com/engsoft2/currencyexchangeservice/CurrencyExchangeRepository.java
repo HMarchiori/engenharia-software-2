9e67438d2cc3d8c7dd982d91043f872681c6541a
EduardoSBarbosa <eduardosb367@gmail.com>
2025-11-17 00:31:16 -0300

    Revert "Versão final do projeto"
    This reverts commit 3b332fe83a16a9757004abb2a59a29f1320eb72f.

--- es2-DalbemFuncional/currency-exchange-service/src/main/java/com/engsoft2/currencyexchangeservice/CurrencyExchangeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.engsoft2.currencyexchangeservice;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CurrencyExchangeRepository extends CrudRepository<CurrencyExchange,Long> {
-    CurrencyExchange findByFromAndTo(String from, String to);
-}
